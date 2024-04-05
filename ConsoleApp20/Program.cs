5e7bb873149835ebb5153cb4482eb62722d183a2
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2024-04-05 17:27:30 +0300

    done

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public static class StringExtensions
+{
+    public static int WordCount(this string str)
+    {
+        // Split the string by whitespace characters
+        string[] words = str.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+        // Return the count of the words
+        return words.Length;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = "This is a sample sentence with 7 words.";
+
+        Console.WriteLine($"Number of words in the text: {text.WordCount()}");
+    }
+}
