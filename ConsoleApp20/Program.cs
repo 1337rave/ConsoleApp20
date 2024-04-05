93915acc5f3a791198756e3da8ef7420f7a6cc7f
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2024-04-05 17:41:26 +0300

    done

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public static class ArrayExtensions
+{
+    public static int[] Filter(this int[] array, Func<int, bool> predicate)
+    {
+        // Initialize a dynamic list to store filtered elements
+        var filteredList = new System.Collections.Generic.List<int>();
+
+        // Iterate through the array and apply the predicate to each element
+        foreach (int element in array)
+        {
+            // If the predicate evaluates to true for the current element, add it to the filtered list
+            if (predicate(element))
+            {
+                filteredList.Add(element);
+            }
+        }
+
+        // Convert the list to an array and return
+        return filteredList.ToArray();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        // Predicate for filtering even numbers
+        Func<int, bool> isEven = n => n % 2 == 0;
+
+        // Filter even numbers
+        int[] evenNumbers = numbers.Filter(isEven);
+
+        Console.WriteLine("Even numbers:");
+        foreach (int num in evenNumbers)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+
+        // Predicate for filtering odd numbers
+        Func<int, bool> isOdd = n => n % 2 != 0;
+
+        // Filter odd numbers
+        int[] oddNumbers = numbers.Filter(isOdd);
+
+        Console.WriteLine("Odd numbers:");
+        foreach (int num in oddNumbers)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+}
