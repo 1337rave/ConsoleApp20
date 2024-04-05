dc72a26f33fb8470f6d305beac54f91e7877eed9
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2024-04-05 17:14:04 +0300

    done

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public static class IntExtensions
+{
+    public static bool IsOdd(this int number)
+    {
+        return number % 2 != 0;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1 = 5;
+        int num2 = 10;
+
+        Console.WriteLine($"{num1} is odd: {num1.IsOdd()}");
+        Console.WriteLine($"{num2} is odd: {num2.IsOdd()}");
+    }
+}
