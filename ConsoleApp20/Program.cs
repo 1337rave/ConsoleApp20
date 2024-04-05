e79a36355136b5de00597bb0d9d2d7d8761faaf9
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2024-04-05 17:16:15 +0300

    done

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public static class IntExtensions
+{
+    public static bool IsEven(this int number)
+    {
+        return number % 2 == 0;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1 = 6;
+        int num2 = 11;
+
+        Console.WriteLine($"{num1} is even: {num1.IsEven()}");
+        Console.WriteLine($"{num2} is even: {num2.IsEven()}");
+    }
+}
