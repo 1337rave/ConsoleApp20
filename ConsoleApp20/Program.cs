b4bcc32e96a2e067f727339a9c8467cb303af6ce
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2024-04-05 17:19:49 +0300

    done

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public static class StringExtensions
+{
+    public static int CountSentences(this string line)
+    {
+        int count = 0;
+        foreach (char c in line)
+        {
+            if (c == '.' || c == '!' || c == '?')
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = "This is a sentence. And this is another! Finally, a third?";
+
+        Console.WriteLine($"Number of sentences in \"{text}\": {text.CountSentences()}");
+    }
+}
